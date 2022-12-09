5d3c8e04df0e9d5573b08db6225eb32c5d9ffe3f
Tiffany <thlc@uw.edu>
2022-12-08 16:37:53 -0800

    edit scatterplot variables + added caption + text to intro

--- shiny/app_server.R
+++ shiny/app_server.R
@@ -6,31 +6,63 @@ library(ggplot2)
 
 ## read data
 emissions_df <- read.csv("/Users/tiffanychung/Documents/info201/Assignments/a5-tiffanyhlc/data/owid-co2-data.csv")
-View(emissions_df)
+#View(emissions_df)
 
-raw_codebook <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-codebook.csv")
-View(raw_codebook)
+emissions_df <- emissions_df %>% select( c("country", "year" , "co2" , "co2_per_capita" , "co2_growth_prct" ,"gdp", "population" )) %>%
 
-## trim datasets
+mutate("CO2 Emissions" = co2, "CO2 Emissions per Capita" = co2_per_capita, "CO2 Growth Percentage" = co2_growth_prct, "GDP" = gdp, "Population" = population)
+       
+#filter( ! (country %in% c("World", "Europe" ,"Europe (GCP)", "Europe (excl. EU-27)"), "High-income countries",
+#       "Europe (excl. EU-28)" ,"European Union (27)", "European Union (27) (GCP)" , "European Union (28)") )
+
+###look at the country's name
+#emissions_df %>% pull("country") %>% unique()
+
+
+#raw_codebook <- read.csv("https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-codebook.csv")
+#View(raw_codebook)
+
+
+emissions_df %>% filter(year==2018) %>% filter(co2==max(co2, na.rm=TRUE) ) %>% pull("country")
+
 
 ## server
+options(scipen=20)
 server <- function(input, output) {
+  
+emissions_df_summary <- emissions_df %>%
+group_by(country) %>%
+filter( year==max(year) ) %>%
+ungroup()
+
 #summary values (3)
-  output$summary <-renderPrint({
-    emissions_df %>%
-    summarize(
-    mean_co2 = mean(co2, na.rm = TRUE),
-    mean_co2_percapita = mean(co2_per_capita, na.rm = TRUE),
-    mean_co2_growth = mean(co2_growth_prct, na.rm = TRUE)
+output$summary <-renderPrint({
+emissions_df_summary %>%
+summarize(
+min_co2 = min(co2, na.rm = TRUE),
+mean_co2 = mean(co2, na.rm = TRUE),
+max_co2 = mean(co2, na.rm = TRUE),
+min_co2_percapita = min(co2_per_capita, na.rm = TRUE),
+mean_co2_percapita = mean(co2_per_capita, na.rm = TRUE),
+max_co2_percapita = max(co2_per_capita, na.rm = TRUE),
+min_co2_growth = min(co2_growth_prct, na.rm = TRUE),
+mean_co2_growth = mean(co2_growth_prct, na.rm = TRUE),
+max_co2_growth = max(co2_growth_prct, na.rm = TRUE)
       )
   })
   
-  regFormula <- reactive({
-    as.formula(paste(input$y ,'~', input$x))
+regFormula <- reactive({
+as.formula(paste0("`",input$y, "`" ,'~', "`" , input$x, "`" ))
   })
   
-  output$scatter <- renderPlot({
-    plot(regFormula(), emissions_df , pch=19)
-  }
-      )
-  }
+emissions_df_scatter <-reactive({ emissions_df[emissions_df$year==as.numeric(input$year),] })
+
+title_scatter <- reactive(paste(input$x, "vs", input$y, "in" , input$year))
+output$scatter <- renderPlot({
+  plot(regFormula(), emissions_df_scatter() , main=title_scatter(), pch=19)
+  })
+output$scatter_caption <- renderText(paste("This visualization is a scatterplot that
+                                     displays the trends of countries'", input$y, 
+                                     "based on their", input$x, "by year", input$year))
+}
+
--- shiny/app_ui.R
+++ shiny/app_ui.R
@@ -3,39 +3,53 @@
 #libraries
 library(shiny)
 library(dplyr)
+library(shinythemes)
 
 introduction_page <- tabPanel(
   "Introduction",
+  shinythemes::themeSelector(),
+  fluidPage(theme=shinytheme("flatly")),
   titlePanel("Introduction"),
-  h3("*Analyzed Variables*"),
+  h3("About the Variables"),
+  h4("CO2 allows us to know how many tons of CO2 are emitteed per country, the
+     co2 growth lets us know if the co2 emissions are increasing or decreasing.
+     The co2 per capital is to get the average emission per person."),
   h4("*my analysis*"),
-  h3("*Values*"),
-  h4("*This table displays the average of co2, co2 per capital, and 
-     co2 growth.*"),
+  h4("*This table displays the minimum, average, and maximum values in co2,
+  co2 per capital, and co2 growth.*"),
   #display the summary table (averages)
   mainPanel(
-  verbatimTextOutput("summary")
+  verbatimTextOutput("summary"),
+  h4("On average, a country emits 4.7 tons of co2 a year per person. 
+The country with lowest emissions per capita is ___ with 0.027 tons per person ; 
+and the country with the highest emissions per capita was ___ with 35.6 tons per person.
+")
   )
 )
 
+
+
 interactive_viz <- tabPanel(
   "CO2 Chart",
-  titlePanel("CO2 Chart"),
-  h5("*caption*"),
+  titlePanel("Trends in CO2 Emissions"),
   sidebarLayout(
     sidebarPanel(
+      selectInput("year","Select year:",
+                   choices = c("1990","2000", "2005", "2015", "2018"),
+                   selected= "2018"),
       selectInput("x",
-                  "Variable on the X axis",
-                  choices = c("co2","gdp")),
+                  "X axis variable:",
+                  choices = c("CO2 Emissions","GDP", "CO2 Growth Percentage", "Population")),
       selectInput("y",
-                  "Variable on the Y axis",
-                  choices = c("co2","gdp"))
+                  "Y axis variable:",
+                  choices = c("CO2 Emissions","GDP","CO2 Growth Percentage", "Population"))
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("scatter")
-    )
+      plotOutput("scatter"),
+      textOutput("scatter_caption")
+    ),
   )
 )
   
